4e2b6199138685ca61af4bb599ece80c538602f4
PorganyiKristof <75934852+PorganyiKristof@users.noreply.github.com>
2024-04-10 12:39:40 +0200

    vercel fix

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -9,7 +9,7 @@ export default function HeroSection() {
         <div className="col-span-7 place-self-center text-center sm:text-left">
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-800 via-purple-400-600 to-pink-500 hover:bg-slate-300">
-              Hello, I`m
+              Hello, I&apos;m
             </span>
             <br />
             <TypeAnimationComponent />
