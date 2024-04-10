edc53b51a1f1a417cbcb49ead5d80835d373601c
PorganyiKristof <75934852+PorganyiKristof@users.noreply.github.com>
2024-04-10 14:59:23 +0200

    email fix

--- app/api/send/route.jsx
+++ app/api/send/route.jsx
@@ -1,6 +1,12 @@
 const nodemailer = require("nodemailer");
+import { Tailwind } from "@react-email/components";
 import { render } from "@react-email/render";
+import Image from "next/image";
 import { NextResponse } from "next/server";
+
+const Email = () => {
+  return <Tailwind>{children}</Tailwind>;
+};
 
 export async function POST(_req, _res) {
   const { email, subject, message } = await _req.json();
@@ -27,17 +33,62 @@ export async function POST(_req, _res) {
         }
       });
     });
-    const emailHtml = render(<div>{message}</div>);
+    const emailHtmlToTheSender = render(
+      <div className="flex bg-black text-white w-full">
+        <div className="flex flex-col  justify-center m-auto">
+          <h1 className="font-bold uppercase border-b border-purple-500 mb-4">
+            {subject}
+          </h1>
+          <div className="mb-4">
+            <p>Thank you for contacting us!</p>
+            <p>New message submitted:</p>
+          </div>
+          <p>&quot;{message}&quot;</p>
+        </div>
+        {/* <Image
+          src="/images/hero-image.png"
+          alt="hero-image"
+          width="300"
+          height="300"
+        /> */}
+      </div>
+    );
+    const emailHtmlForMe = render(
+      <div>
+        <p>{subject}</p>
+        <p>{message}</p>
+      </div>
+    );
 
     const options = {
       from: `From <${process.env.google_user}>`,
       to: email,
       subject: subject,
-      html: emailHtml,
+      html: emailHtmlToTheSender,
     };
+
+    const optionsForMe = {
+      from: `From <${process.env.google_user}>`,
+      to: process.env.google_user,
+      subject: subject,
+      html: emailHtmlForMe,
+    };
+
     await new Promise((resolve, reject) => {
       // send mail
       transporter.sendMail(options, (err, info) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        } else {
+          console.log(info);
+          resolve(info);
+        }
+      });
+    });
+    await new Promise((resolve, reject) => {
+      // send mail
+      transporter.sendMail(optionsForMe, (err, info) => {
         if (err) {
           console.error(err);
           reject(err);
--- app/page.jsx
+++ app/page.jsx
@@ -9,6 +9,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar />
+
       <div className="container mx-auto px-12 py-8 mt-24">
         <HeroSection />
         <AchievementsSection />
