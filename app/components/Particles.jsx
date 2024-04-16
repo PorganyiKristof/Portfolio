b40275e3ec6c45aec4d5031df6befdbfbd679a87
PorganyiKristof <75934852+PorganyiKristof@users.noreply.github.com>
2024-04-16 10:09:58 +0200

    Update Particles.jsx

--- app/components/Particles.jsx
+++ app/components/Particles.jsx
@@ -2,21 +2,13 @@
 import React, { useEffect, useMemo, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { loadSlim } from "@tsparticles/slim";
-import { AnimatePresence, motion } from "framer-motion";
 
 export default function ParticlesBG() {
   const [init, setInit] = useState(false);
 
-  // this should be run only once per application lifetime
   useEffect(() => {
     initParticlesEngine(async (engine) => {
-      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-      // starting from v2 you can add only the features you need reducing the bundle size
-      //await loadAll(engine);
-      //await loadFull(engine);
       await loadSlim(engine);
-      //await loadBasic(engine);
     }).then(() => {
       setInit(true);
     });
@@ -103,20 +95,12 @@ export default function ParticlesBG() {
 
   if (init) {
     return (
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        tranform={{ delay: 2 }}
-      >
-        <AnimatePresence>
-          <Particles
-            id="tsparticles"
-            particlesLoaded={particlesLoaded}
-            options={options}
-            className="w-full h-full absolute "
-          />
-        </AnimatePresence>
-      </motion.div>
+      <Particles
+        id="tsparticles"
+        particlesLoaded={particlesLoaded}
+        options={options}
+        className="w-full h-full absolute "
+      />
     );
   }
 }
